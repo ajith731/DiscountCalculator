3ed5e6d5e98529470cdca845be111d2efc2f6111
ajith731 <ajithdineshan@gmail.com>
2025-09-18 17:36:48 +0530

    msg

--- rbuild/src/main.jsx
+++ rbuild/src/main.jsx
@@ -1,10 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import './index.css'
-import './index.html'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
